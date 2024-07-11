0380717e183c777cbdebc011d224bdc5938d0b07
Montalto <montaltomarcos@gmail.com>
2024-07-10 21:59:37 -0300

    melhoria no codigo

--- Mundo1/Calculos e operacoes/desafio3.py
+++ Mundo1/Calculos e operacoes/desafio3.py
@@ -1,7 +1,9 @@
 # Crie um algoritmo que leia um número e mostre o seu dobro, o seu triplo e sua raiz quadrada:
 
+from math import sqrt
+
 n = int(input('Digite um numero inteiro: \n'))
 
 print(f'O dobro do numero e {n * 2}')
 print(f'O triplo do numero e {n * 3}')
-print(f'A raiz quadrada do numero e {n ** (1/2):.2f}')
+print(f'A raiz quadrada do numero e {sqrt(n):.2f}')
